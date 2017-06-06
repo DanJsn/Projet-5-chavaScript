1db7d1e1f4f7ff1da7f0e92bcdd2e024bdac9545
Danjsn <djansoone@gmail.com>
2017-06-06 10:18:15 +0200

    0606

---
+++ Site perso/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/master.css">
+    <title>Jobz</title>
+  </head>
+  <body>
+
+    <div class="header">
+
+      <img src="img/Jobz_logo.jpg" alt="Jobz" class="jobz_logo">
+      <ul class="header_menu">
+        <li>Who i am?</li>
+        <li>Music</li>
+        <li>Store</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+
+      <ul class="header_social">
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>Twitter</li>
+        <li>Soundcloud</li>
+      </ul>
+
+    </div>
+  </body>
+</html>
---
+++ layout_exercice/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Turlututu</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="container">
+
+
+<header>
+      <div class="header-p">
+        <div class="header_container">
+          <img src="./img/water.png" alt="water" class="water">
+              <div class="header_gauche">
+                <ul>
+                  <li>Features</li>
+                  <li>Docs</li>
+                  <li>Blog</li>
+                  <li>Templates</li>
+                  <li>Pricing</li>
+                  <li>Contact</li>
+                  <li>Sign up</li>
+                </ul>
+              </div>
+
+      <div class="header_droite">
+        <ul>
+          <li>Log in</li>
+        </ul>
+      </div>
+</div>
+</header>
+
+<div class="intro">
+
+    <div class="intro_container">
+
+    <div class="intro_info">
+      <img src="./img/turlututu.png" alt="turlututu" class="turlututu_img">
+    <h1>The Plug And Play Database service</h1>
+    <br>
+    <a href="#" class="btn btn-primary btn-lg active" role="button"> Start building now </a>
+    </div>
+    </div>
+
+</div>
+
+    <div class="break_1">
+      <p>For awesome projects with deadlines</p>
+    </div>
+
+    <div class="presentation">
+      <div class="presentation_text_1">
+        <h2>Simple to use</h2>
+        <p>Turlututu.io is probably the fastest solution to get a database going for web and serveless applications. Model your quickly. The data management application, schema and REAST API are Instantly avaible.</p>
+      </div>
+     <div class="presentation_text_2">
+      <h2>Productive</h2>
+      <p>Collaborate and work with data in a workspace on your desktop, laptop or smartphone. Quickly search through all your data. Secure access with roles.</p>
+     </div>
+
+      <div class="presentation_text_3">
+        <h2>Built for the modern web</h2>
+        <p>Designed for the moder web with high performance REST API and NoSQL, MangoDB query language, server-side codehooks, users authentification (Aush0).
+        Media Archive, Realtime message and secure Ajax/CDRS.</p>
+      </div>
+
+      <div class="presentation_text_4">
+        <h2>More than a database</h2>
+        Turlututu.com databases can also serve web pages. Use HTML, Javascript, jQuery, Angular, ReactJS( or your favorite framework) on turlututu.com Pages with server-side data-blinding.
+      </div>
+    </div>
+
+    <div class="break_02">
+      <h3>innovative teams gets more done with turlututu.com</h3>
+    </div>
+
+    <div class="logo">
+
+      <div class="chatting">
+        <img src="./img/chatting.png" alt="chatting">
+      </div>
+
+      <div class="diagram">
+        <img src="./img/diagram.png" alt="diagram">
+      </div>
+
+      <div class="maps">
+        <img src="./img/maps.png" alt="maps">
+      </div>
+
+      <div class="wifi">
+          <img src="./img/wifi.png" alt="wifi">
+      </div>
+
+    </div>
+
+    <div class="final">
+        <ul>
+          <li>Rest API</li>
+          <li>Docs</li>
+          <li class="li_orange">Start bulding</li>
+        </ul>
+    </div>
+
+    <footer>
+      <div class="footer_gauche">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Terms & conditions</li>
+          <li>Private Policy</li>
+        </ul>
+      </div>
+
+      <div class="footer_droit">
+        <p>@2017 turlututu</p>
+      </div>
+    </footer>
+  </div>
+  </body>
+</html>
